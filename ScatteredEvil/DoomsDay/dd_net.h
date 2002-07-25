c1b899554c802db7ed25983b2c942bd2b0b637de
Jānis Legzdiņš <769407+janisl@users.noreply.github.com>
2002-07-25 18:11:47 +0000

    Merged everything into a single exe.

--- ScatteredEvil/DoomsDay/dd_net.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DOOMSDAY_NETWORK_H__
-#define __DOOMSDAY_NETWORK_H__
-
-void D_ArbitrateNetStart (void);
-void D_CheckNetGame (void);
-void D_QuitNetGame (void);
-
-#endif
--- ScatteredEvil/sed3d.bat
+++
@@ -1,1 +0,0 @@
-@doomsday -game ScatteredEvil.dll -gl drd3d.dll %1 %2 %3 %4 %5 %6 %7 %8 %9
