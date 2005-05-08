e81bf1d8a6b59665ce46b2a23e1a46be37c15c0a
firebrand_kh <firebrand_kh@03699d6e-f511-0410-ba90-c0347c1aabd3>
2005-05-08 00:02:43 +0000

    Fixed to compile and to use vlumpy.exe to create the wad0.wad

--- trunk/karena/progs/compile.bat
+++ trunk/karena/progs/compile.bat
@@ -1,10 +1,8 @@
-c:
-cd \vavoom_src\progs\korax_arena
-..\..\utils\bin\vcc -I../common -Icommon client/clprogs.vc clprogs.dat
-..\..\utils\bin\vcc -I../common -Icommon server/svprogs.vc svprogs.dat
-copy *.dat C:\va_115_2\basev\karena\progs
-cd \va_115_2\basev\karena
+vcc -I../common -Icommon client/clprogs.vc clprogs.dat
+vcc -I../common -Icommon server/svprogs.vc svprogs.dat
+@ECHO OFF
+pause
+cd..
 vlumpy wad0.ls
-copy wad0.wad c:\vavoom\basev\karena
-copy wad0.wad c:\vavoom\vavoom\basev\karena
-cd \vavoom_src\progs\korax_arena
+copy wad0.wad "F:\Korax' Heritage\KArena\karena\"
+cd progs
