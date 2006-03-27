9a3d2481b5e6d2013d0381923a23680ba84ee096
Francisco Ortega <Firebrand-mx@users.noreply.github.com>
2006-03-27 21:47:07 +0000

    Updated to the latest way of compiling

--- karena/progs/compile.bat
+++ karena/progs/compile.bat
@@ -1,9 +1,9 @@
-vcc -I../../progs/common -P../../basev/common/progs hexndefs.vc shared.dat
-vcc -I../../progs/common -P../../basev/common/progs clprogs.vc clprogs.dat
-vcc -I../../progs/common -P../../basev/common/progs svprogs.vc svprogs.dat
+vcc -I../common -P../common hexndefs.vc shared.dat
+vcc -I../common -P../common clprogs.vc clprogs.dat
+vcc -I../common -P../common svprogs.vc svprogs.dat
 @ECHO OFF
 pause
 cd..
 vlumpy wad0.ls
-copy wad0.wad "F:\Korax' Heritage\KArena\karena\"
+copy wad0.wad "D:\Korax\KArena\karena\"
 cd progs
