be95a7d4b403f4e567c010cd9e3d5f7dff7e4e6a
firebrand_kh <firebrand_kh@03699d6e-f511-0410-ba90-c0347c1aabd3>
2006-03-27 16:16:29 +0000

    Initial Import

---
+++ trunk/karena/common/compile.bat
@@ -0,0 +1,7 @@
+vcc common.vc common.dat
+vcc clcommon.vc clcommon.dat
+vcc svcommon.vc svcommon.dat
+@ECHO OFF
+pause
+vlumpy wad0.ls
+copy wad0.wad "D:\Korax\KArena\basev\common"
---
+++ trunk/karena/common/wad0.ls
@@ -0,0 +1,5 @@
+$label pr_start
+common.dat
+clcommon.dat
+svcommon.dat
+$label pr_end
