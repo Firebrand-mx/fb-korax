1a546ecc7124e2b7f9d74163383fb75a4c3e121e
Francisco Ortega <Firebrand-mx@users.noreply.github.com>
2006-05-29 00:52:32 +0000

    Added creation of common wad0.wad

--- karena/progs/compile.bat
+++ karena/progs/compile.bat
@@ -1,6 +1,14 @@
 vcc -I../common -P../common ../common/common.vc ../common/common.dat
 vcc -I../common -P../common ../common/clcommon.vc ../common/clcommon.dat
 vcc -I../common -P../common ../common/svcommon.vc ../common/svcommon.dat
+@ECHO OFF
+cd..
+cd common
+vlumpy wad0.ls
+copy wad0.wad "H:\Korax\KArena\basev\common\"
+cd..
+cd progs
+@ECHO ON
 
 vcc -I../common -P../common hexndefs.vc shared.dat
 vcc -I../common -P../common clprogs.vc clprogs.dat
