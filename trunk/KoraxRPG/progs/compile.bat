622333ff9b0c8b97abc221ddfdca200c24222cc2
crimsonwizard <crimsonwizard@03699d6e-f511-0410-ba90-c0347c1aabd3>
2007-08-13 21:25:31 +0000

    Importing recent Hexen progs.

--- trunk/KoraxRPG/progs/compile.bat
+++ trunk/KoraxRPG/progs/compile.bat
@@ -1,2 +1,20 @@
-vcc -I../common -Icommon client/clprogs.vc clprogs.dat
-vcc -I../common -Icommon server/svprogs.vc svprogs.dat
+vcc -I../common -P../common ../common/engine/classes.vc ../common/engine.dat
+vcc -I../common -P../common ../common/linespec/classes.vc ../common/linespec.dat
+@ECHO OFF
+cd..
+cd common
+vlumpy basepak.ls
+copy basepak.pk3 "<PUT YOUR PATH HERE>\KoraxRPG\basev\common\"
+cd..
+cd progs
+@ECHO ON
+
+vcc -I../common -P../common hexndefs.vc shared.dat
+vcc -I../common -P../common svprogs.vc svprogs.dat
+vcc -I../common -P../common clprogs.vc clprogs.dat
+@ECHO OFF
+pause
+cd..
+vlumpy basepak.ls
+copy basepak.pk3 "<PUT YOUR PATH HERE>\KoraxRPG\koraxrpg\"
+cd progs
