e42e621be4f40c2a3ebd3f8081d8dc7623e59f35
crimsonwizard <crimsonwizard@03699d6e-f511-0410-ba90-c0347c1aabd3>
2007-09-13 09:04:56 +0000

    Initial import: New Thing Types.txt Fixed compile.bat.

--- trunk/KoraxRPG/progs/compile.bat
+++ trunk/KoraxRPG/progs/compile.bat
@@ -1,20 +1,21 @@
-vcc -I../common -P../common ../common/engine/classes.vc ../common/engine.dat
-vcc -I../common -P../common ../common/linespec/classes.vc ../common/linespec.dat
+vcc -I../common -P../common/progs ../common/engine/classes.vc ../common/progs/engine.dat
+vcc -I../common -P../common/progs ../common/linespec/classes.vc ../common/progs/linespec.dat
 @ECHO OFF
 cd..
 cd common
 vlumpy basepak.ls
-copy basepak.pk3 "<PUT YOUR PATH HERE>\KoraxRPG\basev\common\"
+copy basepak.pk3 "<TYPE YOUR PATH HERE>\KoraxRPG\basev\common\"
 cd..
 cd progs
 @ECHO ON
 
-vcc -I../common -P../common shared.vc shared.dat
-vcc -I../common -P../common svprogs.vc svprogs.dat
-vcc -I../common -P../common clprogs.vc clprogs.dat
+vcc -I../common -P../common/progs shared.vc shared.dat
+vcc -I../common -P../common/progs svprogs.vc svprogs.dat
+vcc -I../common -P../common/progs clprogs.vc clprogs.dat
 @ECHO OFF
 pause
 cd..
 vlumpy basepak.ls
-copy basepak.pk3 "<PUT YOUR PATH HERE>\KoraxRPG\koraxrpg\"
+copy basepak.pk3 "<TYPE YOUR PATH HERE>\KoraxRPG\koraxrpg\"
 cd progs
+pause
