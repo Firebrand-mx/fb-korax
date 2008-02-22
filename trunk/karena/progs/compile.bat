373485951cf1297cf9628064fd2ab9895c4d6778
firebrand_kh <firebrand_kh@03699d6e-f511-0410-ba90-c0347c1aabd3>
2008-02-22 22:45:32 +0000

    Generalized the KA directories, fixed directories for common progs

--- trunk/karena/progs/compile.bat
+++ trunk/karena/progs/compile.bat
@@ -1,21 +1,21 @@
-vcc -P../common ../common/engine/classes.vc ../common/engine.dat
-vcc -P../common ../common/linespec/classes.vc ../common/linespec.dat
-vcc -P../common ../common/uibase/classes.vc ../common/uibase.dat
+vcc -P../common/progs/ ../common/engine/classes.vc ../common/progs/engine.dat
+vcc -P../common/progs/ ../common/linespec/classes.vc ../common/progs/linespec.dat
+vcc -P../common/progs/ ../common/uibase/classes.vc ../common/progs/uibase.dat
 @ECHO OFF
 cd..
 cd common
 vlumpy basepak.ls
-copy basepak.pk3 "D:\Korax\KArena\basev\common\"
+copy basepak.pk3 "Z:\KA_DIRECTORY\basev\common\"
 cd..
 cd progs
 @ECHO ON
 
-vcc -I../common -P../common shared.vc shared.dat
-vcc -I../common -P../common svprogs.vc svprogs.dat
-vcc -I../common -P../common clprogs.vc clprogs.dat
+vcc -I../common/progs/ -P../common/progs/ shared.vc shared.dat
+vcc -I../common/progs/ -P../common/progs/ svprogs.vc svprogs.dat
+vcc -I../common/progs/ -P../common/progs/ clprogs.vc clprogs.dat
 @ECHO OFF
 pause
 cd..
 vlumpy basepak.ls
-copy basepak.pk3 "D:\Korax\KArena\karena\"
+copy basepak.pk3 "Z:\KA_DIRECTORY\karena\"
 cd progs
