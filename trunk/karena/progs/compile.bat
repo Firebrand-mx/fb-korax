d55029ddef1507df70dac3849e9a6693c3ffa594
firebrand_kh <firebrand_kh@03699d6e-f511-0410-ba90-c0347c1aabd3>
2008-09-05 22:35:11 +0000

    Reverted my changes back

--- trunk/karena/progs/compile.bat
+++ trunk/karena/progs/compile.bat
@@ -1,6 +1,6 @@
-vcc -P../../common/ ../../common/engine/classes.vc ../../common/engine.dat
-vcc -P../../common/ ../../common/linespec/classes.vc ../../common/linespec.dat
-vcc -P../../common/ ../../common/uibase/classes.vc ../../common/uibase.dat
+vcc -P../../../basev/common/progs ../../common/engine/classes.vc ../../../basev/common/progs/engine.dat
+vcc -P../../../basev/common/progs ../../common/linespec/classes.vc ../../../basev/common/progs/linespec.dat
+vcc -P../../../basev/common/progs ../../common/uibase/classes.vc ../../../basev/common/progs/uibase.dat
 @ECHO OFF
 cd..
 cd..
@@ -12,13 +12,13 @@ copy basepak.pk3 "D:\Korax\KArena\basev\common\"
 cd..
 cd..
 cd progs
-cd ka_source
+cd karena
 cd progs
 @ECHO ON
 
-vcc -I../../common/linespec -P../../common/progs linespec/classes.vc linespec.dat
-vcc -P../../common/ game/classes.vc svprogs.dat
-vcc -P../../common/ cgame/classes.vc clprogs.dat
+vcc -I../../common/linespec -P../../../basev/common/progs linespec/classes.vc linespec.dat
+vcc -P../../../basev/common/progs game/classes.vc svprogs.dat
+vcc -P../../../basev/common/progs cgame/classes.vc clprogs.dat
 @ECHO OFF
 pause
 cd..
