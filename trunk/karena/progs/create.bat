ade5d8b84e3b9a88e721a4fea31db9eebb7afe98
firebrand_kh <firebrand_kh@03699d6e-f511-0410-ba90-c0347c1aabd3>
2008-09-11 23:12:50 +0000

    Removed compile.bat and changed it for a batch file that only builds the basepak files

---
+++ trunk/karena/progs/create.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+cd..
+cd..
+cd..
+cd basev
+cd common
+vlumpy basepak.ls
+copy basepak.pk3 "D:\Korax\KArena\basev\common\"
+cd..
+cd..
+cd progs
+cd karena
+vlumpy basepak.ls
+pause
+copy basepak.pk3 "D:\Korax\KArena\karena\"
+
