9130dc806525b6b140312ee07cc0d9e5293f3256
firebrand_kh <firebrand_kh@03699d6e-f511-0410-ba90-c0347c1aabd3>
2008-09-11 23:13:28 +0000

    Removed compile.bat and changed it for a batch file that only builds the basepak files

---
+++ trunk/KoraxRPG/progs/create.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+cd..
+cd..
+cd..
+cd basev
+cd common
+vlumpy basepak.ls
+copy basepak.pk3 "D:\Korax\KoraxRPG\basev\common\"
+cd..
+cd..
+cd progs
+cd koraxrpg
+vlumpy basepak.ls
+pause
+copy basepak.pk3 "D:\Korax\KoraxRPG\koraxrpg\"
+
