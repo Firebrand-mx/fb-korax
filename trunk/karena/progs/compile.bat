bbe3216e67e2e23b621126d7cf1808319f4fd7bf
firebrand_kh <firebrand_kh@03699d6e-f511-0410-ba90-c0347c1aabd3>
2008-09-14 19:40:33 +0000

    Re added compile.bat since it's still going to be used

---
+++ trunk/karena/progs/compile.bat
@@ -0,0 +1,27 @@
+vcc -P../../../basev/common/progs ../../common/engine/classes.vc ../../../basev/common/progs/engine.dat
+vcc -P../../../basev/common/progs ../../common/linespec/classes.vc ../../../basev/common/progs/linespec.dat
+vcc -P../../../basev/common/progs ../../common/uibase/classes.vc ../../../basev/common/progs/uibase.dat
+@ECHO OFF
+cd..
+cd..
+cd..
+cd basev
+cd common
+vlumpy basepak.ls
+copy basepak.pk3 "D:\Korax\KArena\basev\common\"
+cd..
+cd..
+cd progs
+cd karena
+cd progs
+@ECHO ON
+
+vcc -I../../common/linespec -P../../../basev/common/progs linespec/classes.vc linespec.dat
+vcc -P../../../basev/common/progs game/classes.vc game.dat
+vcc -P../../../basev/common/progs cgame/classes.vc cgame.dat
+@ECHO OFF
+pause
+cd..
+vlumpy basepak.ls
+copy basepak.pk3 "D:\Korax\KArena\karena\"
+cd progs
