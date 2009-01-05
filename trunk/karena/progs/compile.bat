d18ad51fc0172eb85503119a4b717a31ba77ecb8
firebrand_kh <firebrand_kh@03699d6e-f511-0410-ba90-c0347c1aabd3>
2009-01-05 18:58:36 +0000

    Returned the uncompiled progs version for some tests

--- trunk/karena/progs/compile.bat
+++ trunk/karena/progs/compile.bat
@@ -2,18 +2,6 @@ vcc -P../../../basev/common/progs ../../common/engine/classes.vc ../../../basev/
 vcc -P../../../basev/common/progs ../../common/linespec/classes.vc ../../../basev/common/progs/linespec.dat
 vcc -P../../../basev/common/progs ../../common/uibase/classes.vc ../../../basev/common/progs/uibase.dat
 @ECHO OFF
-cd..
-cd..
-cd..
-cd basev
-cd common
-vlumpy basepak.ls
-copy basepak.pk3 "D:\Korax\KArena\basev\common\"
-cd..
-cd..
-cd progs
-cd karena
-cd progs
 @ECHO ON
 
 vcc -I../../common/linespec -P../../../basev/common/progs linespec/classes.vc linespec.dat
@@ -22,6 +10,9 @@ vcc -P../../../basev/common/progs cgame/classes.vc cgame.dat
 @ECHO OFF
 pause
 cd..
-vlumpy basepak.ls
-copy basepak.pk3 "D:\Korax\KArena\karena\"
+vlumpy d:\korax\vavoom\source\basev\common\basepak.ls
+move basepak.pk3 "D:\Korax\KArena\basev\common\"
+vlumpy basepak2.ls
+move basepak.pk3 "D:\Korax\KArena\karena\"
 cd progs
+pause
