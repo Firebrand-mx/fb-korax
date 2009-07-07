323ae3e5438a0248f152ecabab28b8c0648fd3bd
firebrand_kh <firebrand_kh@03699d6e-f511-0410-ba90-c0347c1aabd3>
2009-07-07 12:17:59 +0000

    Added missing quotation mark to classes.vc. Removing stuff no longer needed. Fixed constant value for bot AI.

--- trunk/karena/progs/create.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-cd..
-vlumpy d:\korax\vavoom\source\basev\common\basepak.ls
-move basepak.pk3 "D:\Korax\KArena\basev\common\"
-vlumpy basepak.ls
-pause
-move basepak.pk3 "D:\Korax\KArena\karena\"
