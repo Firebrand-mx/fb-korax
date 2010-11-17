bf3831a83015479ad5b36c0eb1a219a5822ed2ae
firebrand_kh <firebrand_kh@03699d6e-f511-0410-ba90-c0347c1aabd3>
2010-11-17 02:10:59 +0000

    - Finished the new compilation process, now we build both progs and resource files (PK3s) in a single process. - Fixed a small problem in the Gameplay options menu. - Fixed output of batch files. - Fixed small problems in batch files logic.

--- trunk/KoraxRPG/pk3s/sounds/pk3s.bat
+++ trunk/KoraxRPG/pk3s/sounds/pk3s.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 REM This batch file calls another batch file that handles directory names from the current directory...
 REM (See directory.bat for details on what it does...)
-echo $destfile sounds.pk3>%CD%\sounds.ls
+echo $dest sounds.pk3>%CD%\sounds.ls
+echo.
 
 for /d %%a IN (%CD%\*) do call directory.bat %%a %%~na
