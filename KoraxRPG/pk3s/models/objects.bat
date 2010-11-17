8574fe6998fa5abeed5087b00526c90ab2a7a57b
Francisco Ortega <Firebrand-mx@users.noreply.github.com>
2010-11-17 02:10:59 +0000

    - Finished the new compilation process, now we build both progs and resource files (PK3s) in a single process. - Fixed a small problem in the Gameplay options menu. - Fixed output of batch files. - Fixed small problems in batch files logic.

--- KoraxRPG/pk3s/models/objects.bat
+++ KoraxRPG/pk3s/models/objects.bat
@@ -3,7 +3,7 @@ REM I don't like this, but it seems it must be done... skip .svn directory...
 if %4 == .svn goto :End else goto :Print
 
 :Print
-echo models\objects\%5\%1%2		%4\%1%2>>%CD%\models.ls
+echo models\objects\%5\%1%2		%4>>%CD%\models.ls
 
 :End
 
--- KoraxRPG/progs/compile.bat
+++ KoraxRPG/progs/compile.bat
@@ -1,3 +1,6 @@
+@ECHO OFF
+ECHO Compiling progs...
+@ECHO ON
 vcc -P../../../basev/common/progs ../../common/engine/classes.vc ../../../basev/common/progs/engine.dat
 vcc -P../../../basev/common/progs ../../common/linespec/classes.vc ../../../basev/common/progs/linespec.dat
 vcc -P../../../basev/common/progs ../../common/uibase/classes.vc ../../../basev/common/progs/uibase.dat
@@ -11,9 +14,28 @@ vcc -P../../../basev/common/progs cgame/classes.vc cgame.dat
 pause
 cd..
 cd..
-vlumpy d:\korax\vavoom\source\basev\common\basepak.ls
-move basepak.pk3 "D:\Korax\KoraxRPG\basev\common\"
+ECHO Building common progs basepak.pk3...
+vlumpy C:\korax\vavoom\source\basev\common\basepak.ls
+ECHO Copying compiled  common basepak.pk3 file to binary directory...
+move basepak.pk3 "C:\Korax\KoraxRPG\basev\common\"
 cd koraxrpg
+ECHO Building koraxrpg progs basepak.pk3...
 vlumpy basepak.ls
-move basepak.pk3 "D:\Korax\KoraxRPG\koraxrpg\"
+ECHO Copying compiled koraxrpg basepak.pk3 file to binary directory...
+move basepak.pk3 "C:\Korax\KoraxRPG\koraxrpg\"
+ECHO Done building progs.
+pause
+cls
+ECHO Beginning procces to build resource file lists...
+cd pk3s
+call pk3s
+ECHO Finished building resource file lists...
+pause
+cls
+cd..
+ECHO Beginning process to build resource file PK3s...
+for /f %%a IN ('dir %CD%\pk3s\*.ls /B /A-d') do vlumpy %CD%\pk3s\%%a
+ECHO Copying resource files to binary directory...
+for /f %%a IN ('dir %CD%\*.pk3 /B /A-d') do move %%a C:\Korax\KoraxRPG\koraxrpg
+ECHO Finished, bye.
 pause
