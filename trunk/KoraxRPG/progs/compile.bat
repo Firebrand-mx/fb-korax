ae7b3d25a85424a557ed17df10b75b40e68f9e67
ramborc <ramborc@03699d6e-f511-0410-ba90-c0347c1aabd3>
2017-03-14 21:12:14 +0000

    - abbreviated compiler batch file for progs only (compile.bat) - removed "multiplayer" from main menu - removed credits from help screens array - made "maps" entry in journal redirect to stats screen (testing) - modified values for torch (not working)

--- trunk/KoraxRPG/progs/compile.bat
+++ trunk/KoraxRPG/progs/compile.bat
@@ -1,70 +1,10 @@
 @ECHO OFF
 ECHO Compiling progs...
 @ECHO ON
-vcc -P../../../basev/common/progs ../../common/engine/classes.vc ../../../basev/common/progs/engine.dat
-vcc -P../../../basev/common/progs ../../common/linespec/classes.vc ../../../basev/common/progs/linespec.dat
-vcc -P../../../basev/common/progs ../../common/uibase/classes.vc ../../../basev/common/progs/uibase.dat
-@ECHO OFF
-@ECHO ON
-
-vcc -I../../common/linespec -P../../../basev/common/progs linespec/classes.vc linespec.dat
+vcc -P../../../basev/common/progs ../../../progs/common/engine/classes.vc ../../../basev/common/progs/engine.dat
+vcc -P../../../basev/common/progs ../../../progs/common/linespec/classes.vc ../../../basev/common/progs/linespec.dat
+vcc -P../../../basev/common/progs ../../../progs/common/uibase/classes.vc ../../../basev/common/progs/uibase.dat
+vcc -I../../../progs/common/linespec -P../../../basev/common/progs linespec/classes.vc linespec.dat
 vcc -P../../../basev/common/progs game/classes.vc game.dat
 vcc -P../../../basev/common/progs cgame/classes.vc cgame.dat
 @ECHO OFF
-pause
-cls
-cd..
-cd..
-cd..
-ECHO Building common basepak.ls file...
-cd basev
-cd common
-call pk3s
-cd..
-cd..
-set d=%CD%
-ECHO Building common basepak.pk3...
-vlumpy %CD%\basev\common\basepak.ls
-ECHO Copying compiled common basepak.pk3 file to binary directory...
-move basepak.pk3 %CD%\bin\basev\common\
-cd basev
-cd hexen
-ECHO Building hexen basepak.ls file...
-call pk3s
-cd..
-cd..
-copy %CD%\basev\games.txt %CD%\bin\basev\
-ECHO Building hexen progs basepak.pk3...
-vlumpy %CD%\basev\hexen\basepak.ls
-ECHO Copying compiled hexen basepak.pk3 file to binary directory...
-move basepak.pk3 %CD%\bin\basev\hexen\
-ECHO Building koraxrpg progs basepak.pk3...
-cd code
-cd koraxrpg
-vlumpy %CD%\basepak.ls
-cd..
-cd..
-ECHO Copying compiled koraxrpg basepak.pk3 file to binary directory...
-move %CD%\code\koraxrpg\basepak.pk3 %CD%\bin\koraxrpg\
-ECHO Done building progs.
-pause
-cls
-ECHO Beginning procces to build resource file lists...
-cd code
-cd koraxrpg
-cd pk3s
-call pk3s
-ECHO Finished building resource file lists...
-pause
-cls
-cd..
-ECHO Beginning process to build resource file PK3s...
-for /f %%a IN ('dir %CD%\pk3s\*.ls /B /A-d') do vlumpy %CD%\pk3s\%%a
-ECHO Copying resource files to binary directory...
-cd..
-cd..
-for /f %%a IN ('dir %CD%\code\koraxrpg\*.pk3 /B /A-d') do move %CD%\code\koraxrpg\%%a %CD%\bin\koraxrpg\
-ECHO Copying base.txt to binary directory...
-copy %CD%\code\koraxrpg\base.txt %CD%\bin\koraxrpg\
-ECHO Finished, bye.
-pause
