6e16e17a9fda16f837f2c34e58c6536aca03c055
ramborc <ramborc@03699d6e-f511-0410-ba90-c0347c1aabd3>
2017-03-19 16:53:03 +0000

    Removed shading from Journal screens

--- KoraxRPG/progs/compile.bat
+++ KoraxRPG/progs/compile.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 ECHO Compiling progs...
 @ECHO ON
-vcc -P../../../basev/common/progs ../../../progs/common/engine/classes.vc ../../../basev/common/progs/engine.dat
-vcc -P../../../basev/common/progs ../../../progs/common/linespec/classes.vc ../../../basev/common/progs/linespec.dat
-vcc -P../../../basev/common/progs ../../../progs/common/uibase/classes.vc ../../../basev/common/progs/uibase.dat
+@vcc -P../../../basev/common/progs ../../../progs/common/engine/classes.vc ../../../basev/common/progs/engine.dat
+@vcc -P../../../basev/common/progs ../../../progs/common/linespec/classes.vc ../../../basev/common/progs/linespec.dat
+@vcc -P../../../basev/common/progs ../../../progs/common/uibase/classes.vc ../../../basev/common/progs/uibase.dat
 vcc -I../../../progs/common/linespec -P../../../basev/common/progs linespec/classes.vc linespec.dat
 vcc -P../../../basev/common/progs game/classes.vc game.dat
 vcc -P../../../basev/common/progs cgame/classes.vc cgame.dat
