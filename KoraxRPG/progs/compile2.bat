dc1ec3d6950dbaeedd18d9a61289015efd97d14b
Francisco Ortega <firebrand_2001@yahoo.com.mx>
2017-10-02 18:32:48 -0500

    - Changed compilation batch file, so it deletes temporary files generated by the process.

--- KoraxRPG/progs/compile2.bat
+++ KoraxRPG/progs/compile2.bat
@@ -23,6 +23,13 @@ ECHO Building koraxrpg progs basepak.pk3...
 vlumpy basepak.ls
 ECHO Copying compiled koraxrpg basepak.pk3 file to binary directory...
 move basepak.pk3 "E:\Korax\KoraxRPG\koraxrpg\"
+cd progs
+del cgame.dat
+del cgame.txt
+del game.dat
+del game.txt
+del linespec.dat
+del linespec.txt
 ECHO Done building progs.
 pause
 cls
@@ -35,6 +42,7 @@ cls
 cd..
 ECHO Beginning process to build resource file PK3s...
 for /f %%a IN ('dir %CD%\pk3s\*.ls /B /A-d') do vlumpy %CD%\pk3s\%%a
+for /f %%a IN ('dir %CD%\pk3s\*.ls /B /A-d') do del %CD%\pk3s\%%a
 ECHO Copying resource files to binary directory...
 for /f %%a IN ('dir %CD%\*.pk3 /B /A-d') do move %%a E:\Korax\KoraxRPG\koraxrpg
 ECHO Finished, bye.
